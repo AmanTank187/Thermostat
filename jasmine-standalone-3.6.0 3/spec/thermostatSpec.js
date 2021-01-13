c262d60a846a9edbbf5cca802b7c9ff9267040c9
Chris Abdo <christophermabdo@icloud.com>
2021-01-13 17:33:12 +0000

    spec 6 & 7 done

--- jasmine-standalone-3.6.0 3/spec/thermostatSpec.js
+++ jasmine-standalone-3.6.0 3/spec/thermostatSpec.js
@@ -13,21 +13,44 @@ describe('thermostat', function(){
   describe('Increase function', function() {
     it('will be able to increase the temperature', function() {
       thermostat.increaseTemperature()
-      expect(thermostat._temperature).toEqual(25);
+      expect(thermostat._temperature).toEqual(21);
     });
   });
 
   describe('Decrease function', function() {
     it('will be able to Decrease the temperature', function() {
-      thermostat.decreaseTemperature()
+      for (var i = 0; i < 5; i++) {
+        thermostat.decreaseTemperature();
+        }
+      
       expect(thermostat._temperature).toEqual(15);
     });
 
     it('It cannot go below 10', function(){
-      thermostat.decreaseTemperature()
-      thermostat.decreaseTemperature()
+      for (var i = 0; i < 10; i++) {
+        thermostat.decreaseTemperature();
+        }
       expect(function(){thermostat.decreaseTemperature();}).toThrowError('Cannot decrease below 10');
       expect(thermostat._temperature).toEqual(10);
+    });
+  });
+
+  describe('Power Saving function', function() {
+    it('has a maximum temp of 25 when on', function() {
+      for (var i = 0; i < 5; i++) {
+      thermostat.increaseTemperature();
+      }
+      expect(function(){thermostat.increaseTemperature();}).toThrowError('Power Saving On: Maximum Temperature of 25 Reached');
+      expect(thermostat._temperature).toEqual(25);
+    });
+
+    it('has a maximum temperature of 32 degrees when off', function() {
+      thermostat.powerSavingMode('off')
+      for (var i = 0; i < 12; i++) {
+        thermostat.increaseTemperature();
+        }
+      expect(function(){thermostat.increaseTemperature();}).toThrowError('Maximum Temperature of 32 Reached');
+      expect(thermostat._temperature).toEqual(32);
     });
   });
 });
--- jasmine-standalone-3.6.0 3/src/thermostat.js
+++ jasmine-standalone-3.6.0 3/src/thermostat.js
@@ -4,10 +4,23 @@ class Thermostat{
   constructor(){
     this._temperature = 20;
     this._minTemperature = 10;
+    this.maxTemperature = 25
+    this.powerSaving = true
   }
 
   increaseTemperature() {
-    this._temperature += 5;
+    if (this.powerSaving == true) {
+      if (this._temperature == this.maxTemperature) {
+        throw new Error('Power Saving On: Maximum Temperature of 25 Reached');
+      } else {
+        this._temperature += 1;
+      }
+    } else if (this.powerSaving == false)
+      if (this._temperature == this.maxTemperature) {
+        throw new Error('Maximum Temperature of 32 Reached')
+      } else {
+      this._temperature += 1;
+      }
   }
 
   decreaseTemperature() {
@@ -15,7 +28,17 @@ class Thermostat{
   if(this._temperature == this._minTemperature){
     throw new Error('Cannot decrease below 10');
   } else {
-    this._temperature -= 5;
+    this._temperature -= 1;
+    }
+  }
+ 
+  powerSavingMode(state) {
+    if (state == 'on'){
+      this.powerSaving = true
+      this.maxTemperature = 25
+    } else if (state == 'off') {
+      this.powerSaving = false
+      this.maxTemperature = 32
     }
   }
 }
