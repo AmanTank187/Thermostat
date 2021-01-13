ec268577771d69743bc833c26e85406fde03dd51
Aman Tank <amantank187@gmail.com>
2021-01-13 16:44:01 +0000

    Added feature so the temp cannot go below 10

--- jasmine-standalone-3.6.0 3/spec/thermostatSpec.js
+++ jasmine-standalone-3.6.0 3/spec/thermostatSpec.js
@@ -7,16 +7,27 @@ describe('thermostat', function(){
   });
 
   it('It has a starting temp of 20', function(){
-    expect(thermostat._temperature).toEqual(20); 
+    expect(thermostat._temperature).toEqual(20);
   });
 
   describe('Increase function', function() {
     it('will be able to increase the temperature', function() {
-      thermostat.increaseTemperature(5)
+      thermostat.increaseTemperature()
       expect(thermostat._temperature).toEqual(25);
     });
-
   });
 
-  
+  describe('Decrease function', function() {
+    it('will be able to Decrease the temperature', function() {
+      thermostat.decreaseTemperature()
+      expect(thermostat._temperature).toEqual(15);
+    });
+
+    it('It cannot go below 10', function(){
+      thermostat.decreaseTemperature()
+      thermostat.decreaseTemperature()
+      expect(function(){thermostat.decreaseTemperature();}).toThrowError('Cannot decrease below 10');
+      expect(thermostat._temperature).toEqual(10);
+    });
+  });
 });
--- jasmine-standalone-3.6.0 3/src/thermostat.js
+++ jasmine-standalone-3.6.0 3/src/thermostat.js
@@ -2,10 +2,20 @@
 
 class Thermostat{
   constructor(){
-    this._temperature = 20; 
+    this._temperature = 20;
+    this._minTemperature = 10;
   }
 
   increaseTemperature() {
     this._temperature += 5;
+  }
+
+  decreaseTemperature() {
+
+  if(this._temperature == this._minTemperature){
+    throw new Error('Cannot decrease below 10');
+  } else {
+    this._temperature -= 5;
+    }
   }
 }
